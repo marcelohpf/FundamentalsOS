108b2e351bc8b8a47cfa39db50cac13eb237bebf
Marcelo Ferreira <marcelohpf@gmail.com>
2017-05-07 15:04:47 -0300

    Add mutex to producer and consumer
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- schoolworkThree/src/product_consumer.c
+++ schoolworkThree/src/product_consumer.c
@@ -1,23 +1,73 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "product_consumer.h"
+
 #define PRODUCTOR_REST 100
 #define CONSUMER_REST 150
 
 extern int run;
-void* productor(void* data){
+unsigned int MAX_BUFFER_SIZE = 50;
+pthread_mutex_t locker;
+
+void * productor(void * undefined_data){
+  Data * data = (Data *)undefined_data;
+
   while(run){
-    printf("ola");
+    pthread_mutex_lock(&locker);
+    int number = get_random();
+    if(data->count < MAX_BUFFER_SIZE){
+      data->buffer[data->productor_count] = number;
+      data->count += 1;
+      data->productor_count = (data->productor_count+1)%MAX_BUFFER_SIZE;
+    }else{
+      perror("[fatal]: Buffer full");
+    }
+    if(data->count > data->maxBuffer) data->maxBuffer = data->count;
+    printf("[produtor]: %d\n",number);
+    pthread_mutex_unlock(&locker);
     usleep(PRODUCTOR_REST);
+  }
+
+  printf("fim productor");
+  return NULL;
+}
+
+void * consumer(void * undefined_data){
+  Data * data = (Data *)undefined_data;
+  int first =1;
+  while(run || data->count){
+    int number;
+    pthread_mutex_lock(&locker);
+    if(data->count != 0){
+      number = data->buffer[data->consumer_count];
+      data->consumer_count = (data->consumer_count+1)%MAX_BUFFER_SIZE;
+      data->count -= 1;
+      update_minmax(&data->minmax, number, &first);
+      printf("[consumer]: %d\n",number);
+    }
+    pthread_mutex_unlock(&locker);
+    usleep(CONSUMER_REST);
   }
   printf("fim consumer");
   return NULL;
 }
 
-void* consumer(void* data){
-  while(run){
-    printf("oi");
-    usleep(CONSUMER_REST);
+/* Generate a random number in interval of X=RAND_MAX/2
+ * -X < random_number < X
+ */
+int get_random(){
+  long random_number = random();
+  return random_number - (RAND_MAX/2);
+}
+void update_minmax(MinMax * values, int number, int * first){
+  if(*first){
+    values->minimum = number;
+    values->maximum = number;
+    *first -= 1;
+  } else {
+    values->maximum = (values->maximum < number ? number:values->maximum);
+    values->minimum = (values->minimum > number ? number:values->minimum);
   }
-  printf("fim consumer");
-  return NULL;
 }
--- schoolworkThree/src/product_consumer.h
+++ schoolworkThree/src/product_consumer.h
@@ -1,17 +1,25 @@
 #ifndef PCTHREAD
 #define PCTHREAD
+#define BUFFER_SIZE 50
+
+extern unsigned int MAX_BUFFER_SIZE;
 
 typedef struct MinMax{
   int maximum;
   int minimum;
 } MinMax;
+
 typedef struct Data{
-  MinMax data;
-  int buffer[50];
-  int count;
+  MinMax minmax;
+  int buffer[BUFFER_SIZE];
+  int productor_count;
+  int consumer_count;
   int maxBuffer;
+  int count;
 } Data;
 
-void* productor(void *data);
-void* consumer(void *data);
+void* productor(void *);
+void* consumer(void *);
+void update_minmax(MinMax *, int, int*);
+int get_random();
 #endif

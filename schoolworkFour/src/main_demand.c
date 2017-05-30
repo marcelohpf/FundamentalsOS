12794efe8c5ef5e23355d9319429911f7c56ea2b
Marcelo Ferreira <marcelohpf@gmail.com>
2017-05-30 09:01:57 -0300

    Add library load over demand
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

---
+++ schoolworkFour/src/main_demand.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+void help();
+void execute_dynamic_load(double, char*);
+
+int main(int argc, char* argv[]){
+    if(argc != 3){
+      printf("É exigido 2 parametros\n");
+      help();
+      exit(EXIT_FAILURE);
+    }else if(strcmp(argv[1],"-a")==0){
+      execute_dynamic_load(atof(argv[2]), "arc_sin");
+    }else if(strcmp(argv[1],"-s")==0){
+      execute_dynamic_load(atof(argv[2]), "sin");
+    } else {
+      printf("As operações aceitas são -a|-b");
+    }
+    return 0;
+}
+
+void help(){
+  printf("trigonometry [-a|-s] [value]");
+  printf("\n\t-a arcoseno\n\t-s seno\n");
+  printf("\tvalue valor do angulo em RAD, utilize '.' ao invés de ',' Ex.: 0.221\n");
+}
+
+void execute_dynamic_load(double rads, char * function_name){
+  void * handle = dlopen("libseno.so", RTLD_LAZY);
+  if(!handle) {
+    printf("%s\n", dlerror());
+    exit(EXIT_FAILURE);
+  }
+  dlerror();
+  double (*function)(double);
+  *(void **) (&function) = dlsym(handle, function_name);
+  char *error = dlerror();
+  if(error != NULL){
+    printf("%s\n", error);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("%lf\n", (*function)(rads) );
+  dlclose(handle);
+}

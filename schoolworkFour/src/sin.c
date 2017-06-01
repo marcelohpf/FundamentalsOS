1a12e1fc7812509bc5114526fd258279cb138bea
jpbusche <joaopaulo.busche@gmail.com>
2017-06-01 00:25:53 -0300

    Add functions to sin
    Signed-off-by: jpbusche <joaopaulo.busche@gmail.com>

--- schoolworkFour/src/sin.c
+++ schoolworkFour/src/sin.c
@@ -1,8 +1,33 @@
 #include "sin.h"
+#include <math.h>
+
 double arc_sin(double rads){
-  return rads;
+	double root = sqrt(1 - npow(rads, 2));
+	double angle = arc_tan(rads / root);
+  	return angle;
 }
 
 double sin(double rads){
-  return rads;
+	rads = rads - (npow(rads, 3) / fat(3)) + (npow(rads, 5) / fat(5)) - (npow(rads, 7) / fat(7)) + (npow(rads, 9) / fat(9));
+  	return rads;
+}
+
+double arc_tan(double rads){
+	double angle = rads - (npow(rads, 3) / 3.0) + (npow(rads, 5) / 5.0);
+	return angle;
+}
+
+long int npow(int x, int n){
+	int i, number = 1;
+	for(i = 0; i < n; i++) {
+		number *= x;
+	}
+	return number;
+}
+
+long int fat(int n){
+	if(n >= 1)
+		return n * fat(n - 1);
+	else
+		return 1;
 }
--- schoolworkFour/src/sin.h
+++ schoolworkFour/src/sin.h
@@ -3,5 +3,8 @@
 
 double arc_sin(double);
 double sin(double);
+double arc_tan(double);
+long int npow(int, int);
+long int fat(int);
 
 #endif
